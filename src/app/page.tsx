1cebddf16c682c6d51bcc27964ee18088039e9ad
vruje <vrujeshvasista@gmail.com>
2025-04-23 18:08:48 +0530

    Add dark mode support

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className=" min-h-screen">
       <Container>
-        <div className=" bg-gray-800  rounded-lg shadow-lg p-8">
+        <div className="bg-white dark:bg-gray-800 rounded-lg shadow-lg p-8">
           <PasswordGenerator />
         </div>
       </Container>
